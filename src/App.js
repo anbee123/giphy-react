f0c5f8f61e012cd4babdb8bc19d6575edc57e431
An Bui <anhipchipchip@gmail.com>
2022-12-07 01:39:58 -0500

    gif API

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Button from './components/Button';
 import Giphy from './components/Giphy';
 
 function App() {
-  const [gifData, setGifData] = useState ({});
+  const [gifData, setGifData] =useState({});
 
+  const makeApiCall = async (GiphyFun) => {
+    const randomUrl = `https://api.giphy.com/v1/gifs/random?api_key=KsjTYugNKvEvGBZGQeZms3gf7f92AfFD&tag=&rating=g`
+      
+    const res = await fetch(randomUrl);
+    const json = await res.json();
+
+    setGifData(json)
+  }
+  useEffect(()=>{
+    makeApiCall('random')
+  }, [])
+console.log(gifData)
   return (
     <div className="App">
-     <Button />
-     <Giphy />
+     <Button  />
+     <Giphy gif={gifData}/>
     </div>
   );
 }
--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -1,8 +1,13 @@
-const Giphy = ()=> {
+
+const Giphy = ({gif})=> {
+     console.log(gif)
     return (
         <div className="gif">
 <h1>More Giphy more Fun</h1>
+<h1>{gif.title}</h1>
+<h2>{gif.id}</h2>
         </div>
-    )
+           ) 
+    
 }
 export default Giphy;
